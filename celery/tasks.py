89439c09f94e487702f62ca84f878d4a07ed9004
Maksim <maksnovikov56@gmail.com>
2024-06-14 13:44:13 +0300

    14.06.2024. Add task timer, max_counter, worker max_memmory conf.

--- celery/tasks.py
+++ celery/tasks.py
@@ -3,7 +3,7 @@ from fetch_data import fetch_data
 
 from telegram_helper import bot
 
-@app.task
+@app.task(max_retries=5)
 def check_limits(id):
     bot.send_message(253892073, f"Вы попросили напомнить {id}.")
 
@@ -12,6 +12,13 @@ def schedule_reminders():
     # Пример данных для демонстрации
     results = [1, 2, 3, 5, 6, 7]
     for element in results:
-        check_limits.apply_async((element,))
+        check_limits.apply_async((element,), soft_time_limit=10)
 # celery -A tasks worker --loglevel=info -P eventlet
 # celery -A celery_app beat --loglevel=info
+
+
+@app.task(max_retries=5, time_limit=5)
+def time(id):
+    import time
+    time.sleep(10)
+    bot.send_message(253892073, f"Вы попросили напомнить {id}.")
